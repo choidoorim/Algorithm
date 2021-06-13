85f2a4e983588ab67daabd5c6df7d1c8420efe1d
choidoorim <doorim321@naver.com>
2021-06-13 21:54:00 +0900

    bfs

--- Algorithm/DfsBfs.py
+++ Algorithm/DfsBfs.py
@@ -90,3 +90,27 @@ def ad_list(n, m):
     print(graph[n][m])
 
 
+visited = [False] * 9  # 방문 기록 0 ~ 9
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]  # 0 ~ 9
+
+
+def dfs(graph, v, visited):
+    visited[v] = True   # 현재 노드 방문 처리
+    print(v, end=' ')
+    print(visited)
+    for i in graph[v]:  # 현재 노드와 연결되어 있는 나머지 노드들 확인
+        if not visited[i]:  # 방문처리가 되어 있지 않는 노드들만 확인, 더 이상 방문처리를 할 필요가 없을 경우 재귀함수가 호출되지 않음.
+            dfs(graph, i, visited)
+
+
+dfs(graph, 2, visited)
--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,4 +1,6 @@
-visited = [False] * 9  # 방문 기록 0 ~ 9
+from collections import deque
+
+visited = [False]*9
 graph = [
     [],
     [2, 3, 8],
@@ -9,16 +11,19 @@ graph = [
     [7],
     [2, 6, 8],
     [1, 7]
-]  # 0 ~ 9
+]
 
 
-def dfs(graph, v, visited):
-    visited[v] = True   # 현재 노드 방문 처리
-    print(v, end=' ')
-    print(visited)
-    for i in graph[v]:  # 현재 노드와 연결되어 있는 나머지 노드들 확인
-        if not visited[i]:  # 방문처리가 되어 있지 않는 노드들만 확인, 더 이상 방문처리를 할 필요가 없을 경우 재귀함수가 호출되지 않음.
-            dfs(graph, i, visited)
+def bfs(graph, start, visited):
+    visited[start] = True
+    queue = deque([start])
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
 
 
-dfs(graph, 1, visited)
+bfs(graph, 1, visited)

af01dbf2b1440046a3b67f210c2d04405d5ffbc4
choidoorim <doorim321@naver.com>
2021-06-15 01:07:52 +0900

    bfs, 미로 탈출, 음료수 얼려먹기

--- Algorithm/DfsBfs.py
+++ Algorithm/DfsBfs.py
@@ -55,12 +55,12 @@ def recursive_function(i):
         print(i, '번째에서 종료합니다.')
         return
     print('재귀 함수를 호출 합니다.')
-    recursive_function(i+1)
+    recursive_function(i + 1)
 
 
-def factorial(n):   # 수열
+def factorial(n):  # 수열
     result = 1
-    for i in range(1, n+1):     # 1 ~ n까지
+    for i in range(1, n + 1):  # 1 ~ n까지
         result *= i
     return result
 
@@ -68,10 +68,13 @@ def factorial(n):   # 수열
 def recursive_factorial(n):
     if n <= 1:
         return 1
-    print(recursive_factorial(n-1))
-    return n * recursive_factorial(n-1)
+    print(recursive_factorial(n - 1))
+    return n * recursive_factorial(n - 1)
 
-INF = 999999999 # 무한의 비용
+
+INF = 999999999  # 무한의 비용
+
+
 def ad_matrix():
     graph = [
         [0, 7, 5],
@@ -105,12 +108,83 @@ graph = [
 
 
 def dfs(graph, v, visited):
-    visited[v] = True   # 현재 노드 방문 처리
+    visited[v] = True  # 현재 노드 방문 처리
     print(v, end=' ')
-    print(visited)
     for i in graph[v]:  # 현재 노드와 연결되어 있는 나머지 노드들 확인
         if not visited[i]:  # 방문처리가 되어 있지 않는 노드들만 확인, 더 이상 방문처리를 할 필요가 없을 경우 재귀함수가 호출되지 않음.
             dfs(graph, i, visited)
 
 
-dfs(graph, 2, visited)
+def bfs(graph, start, visited):
+    visited[start] = True
+    queue = deque([start])
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+# 미로 탈출
+def bfs_mirror(x, y):
+    n, m = map(int, input().split())
+
+    graph = []
+    for i in range(n):
+        graph.append(list(map(int, input().split())))
+
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+
+    queue = deque()
+    queue.append((x, y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            print(nx, ny)
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:  # 기존 범위를 벗어나는 경우
+                continue
+            if graph[nx][ny] == 0:  # 몬스터가 있는 경우
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    return graph[n - 1][m - 1]
+
+# 음료수 얼려먹기
+def dfs_ice(x, y):
+    if x < 0 or y < 0 or x >= n_ice or y >= m_ice:
+        return False
+    if graph_ice[x][y] == 0:
+        graph_ice[x][y] = 1
+        dfs_ice(x - 1, y)
+        dfs_ice(x, y - 1)
+        dfs_ice(x + 1, y)
+        dfs_ice(x, y + 1)
+        return True
+    return False
+
+
+def ice():
+    global n_ice, m_ice, graph_ice
+    n_ice, m_ice = map(int, input().split())
+
+    graph_ice = []
+    for i in range(n_ice):
+        graph_ice.append(list(map(int, input().split())))
+
+    count = 0
+    for i in range(n_ice):
+        for j in range(m_ice):
+            if dfs_ice(i, j):
+                count += 1
+    print(count)
+
+
+ice()
+# print(bfs_mirror(0, 0))
+# dfs(graph, 1, visited)
+# bfs(graph, 1, visited)
--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,29 +1,41 @@
 from collections import deque
 
-visited = [False]*9
-graph = [
-    [],
-    [2, 3, 8],
-    [1, 7],
-    [1, 4, 5],
-    [3, 5],
-    [3, 4],
-    [7],
-    [2, 6, 8],
-    [1, 7]
-]
+n, m = map(int, input().split())
+x, y, direction = map(int, input().split())
 
+maps = []
+for i in range(n):
+    maps.append(list(map(int, input().split())))
 
-def bfs(graph, start, visited):
-    visited[start] = True
-    queue = deque([start])
-    while queue:
-        v = queue.popleft()
-        print(v, end=' ')
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
+maps[x][y] = 1
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
+count = 1
+turn_count = 0
+while True:
+    direction -= 1
+    if direction < 0:
+        direction = 3
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    if maps[nx][ny] == 0:
+        print(maps)
+        maps[nx][ny] = 1
+        count += 1
+        x = nx
+        y = ny
+        turn_count = 0
+    else:
+        turn_count += 1
+    if turn_count == 4:
+        nx = x - dx[direction]
+        ny = x - dy[direction]
+        if maps[nx][ny] == 0:
+            x = nx
+            y = ny
+            turn_count = 0
+        else:
+            break
 
-bfs(graph, 1, visited)
+print(count)

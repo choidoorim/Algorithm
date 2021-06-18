efbd25ddb1fb474f0a9003e32b63e8ad2819f8a4
choidoorim <doorim321@naver.com>
2021-06-18 22:17:14 +0900

    quick sort

--- Algorithm/sort.py
+++ Algorithm/sort.py
@@ -39,4 +39,30 @@ def insertion_sort():   # 기준이 되는 수와 바로 전 수를 비교한다
     print(array)
 
 
-insertion_sort()
+# insertion_sort()
+
+
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left = start + 1
+    right = end
+    while left <= right:
+        while left <= end and array[pivot] >= array[left]:  # pivot 값보다 큰 값을 찾기 위한 과정, left 값이 end 값보다 커질 경우 중지
+            left += 1
+            print(left)
+        while right > start and array[pivot] <= array[right]:   # pivot 값보다 작은 값을 찾기 위한 과정, right 값이 start 값보다 작을 경우 중지
+            right -= 1
+            print(right)
+        if left > right:    # 엇갈린 경우 오른쪽의 값과 피봇 값을 변경해준다.
+            array[right], array[pivot] = array[pivot], array[right]
+        else:
+            array[left], array[right] = array[right], array[left]
+    quick_sort(array, start, right - 1)     # pivot 기준으로 왼쪽
+    quick_sort(array, left, end)    # pivot 기준으로 오른쪽
+    print(array)
+
+
+quick_array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+quick_sort(quick_array, 0, len(quick_array) - 1)
--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,10 +1,23 @@
-array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
-
-for i in range(1, len(array)):
-    for j in range(i, 0, -1):
-        if array[j] < array[j-1]:
-            array[j], array[j-1] = array[j-1], array[j]
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+    pivot = start   # 가장 왼쪽 값을 피봇 값으로 설정
+    left = start + 1
+    right = end
+    while left <= right:
+        while left <= end and array[pivot] >= array[left]:  # pivot 값보다 큰 값을 찾기 위한 과정, left 값이 end 값보다 커질 경우 중지
+            left += 1   # pivot 값 보다 클 경우 while 문 탈출
+        while right > start and array[pivot] <= array[right]:   # pivot 값보다 작은 값을 찾기 위한 과정, right 값이 start 값보다 작을 경우 중지
+            right -= 1  # pivot 값 보다 작을 경우 while 문 탈출
+        if left > right:    # 엇갈린 경우 오른쪽의 값과 피봇 값을 변경해준다.
+            array[right], array[pivot] = array[pivot], array[right]
         else:
-            break
+            array[left], array[right] = array[right], array[left]
 
-print(array)
+    quick_sort(array, start, right - 1)     # pivot 기준으로 왼쪽
+    quick_sort(array, left, end)    # pivot 기준으로 오른쪽
+    print(array)
+
+
+quick_array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+quick_sort(quick_array, 0, len(quick_array) - 1)

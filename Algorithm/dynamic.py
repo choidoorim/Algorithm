68518c9611d748cf5a3881b379946bf719e0d28e
choidoorim <doorim321@naver.com>
2021-06-24 01:53:58 +0900

    다이나믹 실전문제 풀이

--- Algorithm/dynamic.py
+++ Algorithm/dynamic.py
@@ -35,3 +35,38 @@ def memoization_fibo():
 
 
 memoization_fibo()
+
+
+def to_one():
+    x = int(input())
+
+    count = 0
+    while x > 1:
+        x = x - 1
+        if x % 5 == 0:
+            x = x // 5
+        if x % 3 == 0:
+            x = x // 3
+        if x % 2 == 0:
+            x = x // 2
+        count += 1
+
+    print(count)
+
+
+def to_one_dynamic():
+    x = int(input())
+
+    d = [0] * 30001
+
+    for i in range(2, x + 1):
+        d[i] = d[i - 1] + 1
+
+        if i % 2 == 0:
+            d[i] = min(d[i], d[i // 2] + 1)
+        if i % 3 == 0:
+            d[i] = min(d[i], d[i // 3] + 1)
+        if i % 5 == 0:
+            d[i] = min(d[i], d[i // 5] + 1)
+
+    print(d[x])
--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,12 +1,15 @@
-d = [0]*100
+x = int(input())
 
-def memoization_fibo(x):
-    if x == 1 or x == 2:
-        return 1
-    if d[x] != 0:   # 만약 이미 계산된적이 있다면 재귀함수를 실행시키지 않도록 함.
-        return d[x]
-    d[x] = memoization_fibo(x - 1) + memoization_fibo(x - 2)
-    return d[x]
+d = [0] * 30
 
+for i in range(2, x + 1):
+    d[i] = d[i - 1] + 1
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i // 2] + 1)
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i // 3] + 1)
+    if i % 5 == 0:
+        d[i] = min(d[i], d[i // 5] + 1)
+    print(d)
 
-print(memoization_fibo(4))
+print(d[x])

c6f156a844b7d4589f4b9159dd1b07ffde45b013
choidoorim <doorim321@naver.com>
2021-06-27 00:53:24 +0900

    다이나믹 실전 문제 풀이

--- Algorithm/dynamic.py
+++ Algorithm/dynamic.py
@@ -104,3 +104,24 @@ def tile():
 
 # tile()
 
+
+def money():
+    n, m = map(int, input().split())
+
+    array = []
+    for i in range(n):
+        array.append(int(input()))
+
+    d = [10001] * (m + 1)
+
+    d[0] = 0
+    for i in range(n):
+        for j in range(array[i], m + 1):
+            if d[j - array[i]] != 10001:
+                d[j] = min(d[j], d[j - array[i]] + 1)
+                print(d)
+
+    print(d[m])
+
+
+money()
--- Algorithm/test.py
+++ Algorithm/test.py
@@ -6,3 +6,11 @@ for i in range(n):
 
 d = [10001] * (m+1)
 
+d[0] = 0
+for i in range(n):
+    for j in range(array[i], m + 1):
+        if d[j - array[i]] != 10001:
+            d[j] = min(d[j], d[j - array[i]] + 1)
+            print(d)
+
+print(d[m])

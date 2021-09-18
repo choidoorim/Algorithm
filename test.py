b6a7d22c5ad604354f3b85d7b65a2eac14c577d9
choidoorim <doorim321@naver.com>
2021-09-19 00:33:40 +0900

    프로그래머스 문제 풀이

--- test.py
+++ test.py
@@ -1,15 +1,19 @@
-import heapq
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    q = [0] * bridge_length
+    # 초마다 다리의 길이중 1칸씩 건널 수 있다.
+    while q:
+        answer += 1
+        q.pop(0)
+        # [0, 7] -> [7, 0] -> [0, 4] .. 다리가 버틸 수 있는 무게를 체크하고 초마다 한칸씩 pop하여 이동
+        if truck_weights:
+            if weight >= sum(q) + truck_weights[0]:
+                q.append(truck_weights.pop(0))
+            else:
+                q.append(0)
+    return answer
 
 
-def solution(scoville, K):
-    count = 0
-    heapq.heapify(scoville)
-    while scoville[0] < K and len(scoville) > 1:    # scovile 배열이
-        num1 = heapq.heappop(scoville)
-        num2 = heapq.heappop(scoville)
-        heapq.heappush(scoville, num1 + (num2 * 2))
-        count += 1
-    return count if scoville[0] >= K else -1
-
-
-print(solution([1, 2, 3, 9, 10, 12], 7))
+# 2개가 올라갈 수 있고 무게를 10kg 까지 견디는 다리
+print(solution(2, 10, [7, 4, 5, 6]))
+print(solution(100, 100, [10]))

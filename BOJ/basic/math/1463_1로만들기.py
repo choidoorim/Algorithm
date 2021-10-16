37c8c7d11f482c2b2d8564e39bd8a181f08300ea
choidoorim <doorim321@naver.com>
2021-10-17 00:43:41 +0900

    백준 문제 풀이

--- BOJ/basic/math/1463_1로만들기.py
+++ BOJ/basic/math/1463_1로만들기.py
@@ -4,7 +4,7 @@ x = int(input())
 dp = [0] * (x + 1)
 for i in range(2, x + 1):
     dp[i] = dp[i - 1] + 1
-    # 2 와 3 으로 나눠지는 수가 있을 수도 있으니
+    # 2 와 3 으로 나눠지는 수가 있을 수도 있으니 둘다 if
     if i % 2 == 0:
         dp[i] = min(dp[i // 2] + 1, dp[i])
     if i % 3 == 0:
